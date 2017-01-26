91d351af2829d783a7baa903164e03d64443a6ef
Jingya Huang <jih201@ucsd.edu>
2017-01-26 09:59:12 -0800

    append an element to projectClick

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -15,4 +15,20 @@ function initializePage() {
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
+}
+
+/* Define the listener function */
+function projectClick(e) {
+	// prevent the page from reloading 
+	e.preventDefault();
+
+	// In an event handler, $(this) refers to 
+	// the object that triggered the event 
+	$(this).css("background-color", "#7fff00");
+
+	var containProject = $(this).closest(".project");
+	containProject.append("<div
+	class='project-description'><p>Descripton of the
+	project.</p></div>");
 }
